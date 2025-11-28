88e9e12364e2b6c214abcc8eb57bf05d7ac6e803
Anbarasu A <124448416+anbarasuacsp@users.noreply.github.com>
2025-11-28 22:48:10 +0530

    Add files via upload

---
+++ fun.js
@@ -0,0 +1,37 @@
+// functions - Aeroic function and non aeroic functions
+
+// non aerroic function
+
+// Function declaration
+
+// function greet(name) {
+
+//     console.log("Hello, " + name + "!");
+// }
+
+// // Calling the function
+// greet("Alice");  // Output: Hello, Alice!
+// greet("Bob");    // Output: Hello, Bob!
+
+// function arithmatic(a, b){
+
+//     console.log(a+b)
+    
+// }
+
+// arithmatic(5,6);
+
+
+var functionName = (a,b) => {
+    console.log(a+b);
+};
+
+
+
+var functionName = (a,b) => {
+    console.log(a-b);
+};
+
+
+
+functionName(5,6);
