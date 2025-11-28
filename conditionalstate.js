0a8d7930352ad6c236b0b521431c0201ddc52139
Anbarasu A <124448416+anbarasuacsp@users.noreply.github.com>
2025-11-28 22:46:54 +0530

    Add files via upload

---
+++ conditionalstate.js
@@ -0,0 +1,55 @@
+// Conditional Statement - if, else, else-if, switch
+
+// if and else condition:
+
+// var  age = 5;
+
+
+// if (age!=10){
+
+//     console.log("a value is greater than 5");
+
+// }else{
+//     console.log("a Value is not greater than 5")
+
+// }
+// else-if , else
+
+// var time = 8;
+
+// if (time > 8) {
+//     console.log(" Time is greater than 8")
+
+// } else if (time === 8) {
+//     console.log(" Time is equals to 8")
+
+// } else if (time < 8) {
+//     console.log("Time is less than 8")
+
+// } 
+
+// Switch
+
+var day = 'a'; 
+
+switch(day){
+    case 1:
+        console.log('This is monday');
+        break;
+    case 2:
+        console.log('This is Tueday');
+        break;
+    case 3: 
+        console.log('This is Wednesday');
+        break;
+    case 4:
+        console.log('This is Thursday');
+        break;
+    case 5:
+        console.log('This is friday');
+        break;
+    default:
+        console.log('Entered value undefined');
+
+}
+
